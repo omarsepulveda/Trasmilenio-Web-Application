fd2b3a633b78104b2f4a8a86ef32d03256eeddc4
omarsepulveda <omar.sepulveda@uptc.edu.co>
2019-03-21 22:23:50 -0500

    Se crea el proyecto integrando Grails y Angular 6

---
+++ Transmilenio/client/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { IndexComponent } from './index/index.component';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { NavService } from './nav/nav.service';
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    IndexComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NgbModule.forRoot()
+  ],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}, NavService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
