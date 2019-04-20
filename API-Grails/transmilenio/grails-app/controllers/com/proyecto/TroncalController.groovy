1a0d25564caad1d9c26acbf90118ea73fd799a55
pc <pc@localhost.localdomain>
2019-04-20 07:57:17 -0500

    Se crea servicio Troncal

---
+++ API-Grails/transmilenio/grails-app/controllers/com/proyecto/TroncalController.groovy
@@ -0,0 +1,64 @@
+package com.proyecto
+
+import grails.validation.ValidationException
+import static org.springframework.http.HttpStatus.*
+
+class TroncalController {
+
+    TroncalService troncalService
+
+    static responseFormats = ['json', 'xml']
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond troncalService.list(params), model:[troncalCount: troncalService.count()]
+    }
+
+    def show(Long id) {
+        respond troncalService.get(id)
+    }
+
+    def save(Troncal troncal) {
+        if (troncal == null) {
+            render status: NOT_FOUND
+            return
+        }
+
+        try {
+            troncalService.save(troncal)
+        } catch (ValidationException e) {
+            respond troncal.errors, view:'create'
+            return
+        }
+
+        respond troncal, [status: CREATED, view:"show"]
+    }
+
+    def update(Troncal troncal) {
+        if (troncal == null) {
+            render status: NOT_FOUND
+            return
+        }
+
+        try {
+            troncalService.save(troncal)
+        } catch (ValidationException e) {
+            respond troncal.errors, view:'edit'
+            return
+        }
+
+        respond troncal, [status: OK, view:"show"]
+    }
+
+    def delete(Long id) {
+        if (id == null) {
+            render status: NOT_FOUND
+            return
+        }
+
+        troncalService.delete(id)
+
+        render status: NO_CONTENT
+    }
+}
---
+++ API-Grails/transmilenio/grails-app/domain/com/proyecto/Troncal.groovy
@@ -0,0 +1,13 @@
+package com.proyecto
+
+class Troncal {
+    String letra_troncal
+    String nombre_troncal
+    String color_troncal
+
+    static constraints = {
+       letra_troncal nullable:false
+       nombre_troncal nullable:false
+       color_troncal nullable:false
+    }
+}
